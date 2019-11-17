0f00401244158e4af11721522ccc73e8b1babca3
Victor Nieves Sanchez <vnievess@gmail.com>
2019-11-17 10:36:59 +0100

    añadida comprobación de contraseña

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,7 @@
 from src import app, DB_PATH, functions
 from flask import render_template, request, redirect, url_for
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,24 +16,26 @@ def login():
             c = conn.cursor()
             c.execute('''PRAGMA foreign_keys = ON;''')  # Parece que no es necesaria esta linea
             details = request.form
-            print(details)
-            query = "SELECT * FROM Usuario WHERE 'username'={} AND 'password'={};".format(details["username"],  hash(details["password"]))
-            print(query)
+            query = "SELECT * FROM Usuario WHERE username='{}'".format(details["username"])
             try:
-                c.execute(query, data_tuple)
-                conn.commit()
+                c.execute(query)
                 result = c.fetchone()
                 print("result", result)
                 if result is None:
-                    return "Usuario no existe TODO" # TODO
-                return redirect(url_for('homepage', username=details["username"]) )
+                    return "Usuario no existe TODO"  # TODO
+                check = check_password_hash(result[2], details["password"])
+                if check:
+                    return redirect(url_for('homepage', username=details["username"]))
+                else:
+                    return "PW NO CASA" # TODO
             except sqlite3.IntegrityError as e:
                 print("Error:", e)
-                return e#render_template('error_sign_in.html', name=details["username"], email=details["email"])
+                return e  # render_template('error_sign_in.html', name=details["username"], email=details["email"])
             except sqlite3.OperationalError as e:
                 print("Error:", e)
-                return e#"Error 503 Service Unavailable.\nPlease try again later"
+                return e  # "Error 503 Service Unavailable.\nPlease try again later"
     return render_template('login.html')
+
 
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
@@ -41,6 +44,7 @@ def sign_in():
             c = conn.cursor()
             c.execute('''PRAGMA foreign_keys = ON;''')  # Parece que no es necesaria esta linea
             details = request.form
+            print("Details:", details)
             date = functions.date_validator(str(details["date"]).replace("/", "-"))
             if not date:
                 return render_template('age_error.html')
@@ -52,9 +56,9 @@ def sign_in():
                 query = "INSERT INTO 'Usuario' ('username', 'contrasena', 'email', 'fecha_nacimiento', nombre," \
                         "'apellidos', 'pais', 'descripcion', 'genero', 'verificado', 'foto') VALUES (?, ?, ?, ?, ?, " \
                         "?, ? , ?, ?, ?, ?)"
-                data_tuple = (details["username"], hash(details["password"]), details["email"], str(details["date"]),
-                              details["firstname"], details["lastname"], details["country"], details["description"],
-                              details["sex"], verified, blob)
+                data_tuple = (details["username"], generate_password_hash(details["password"]), details["email"],
+                              str(details["date"]), details["firstname"], details["lastname"], details["country"],
+                              details["description"], details["sex"], verified, blob)
                 try:
                     c.execute(query, data_tuple)
                     conn.commit()
@@ -68,6 +72,7 @@ def sign_in():
                     return "Error 503 Service Unavailable.\nPlease try again later"
 
     return render_template('sign_up.html')
+
 
 @app.route('/<string:username>/home')
 def homepage(username):
